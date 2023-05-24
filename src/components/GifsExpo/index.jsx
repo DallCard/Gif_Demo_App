762272bb2ce9269a83c55aa92aa61d38cde778f5
Dallana Cardoza Martinez <cardozadallana16@gmail.com>
2023-05-24 09:48:56 -0600

    se crea el UrlList

--- src/components/GifsExpo/index.jsx
+++ src/components/GifsExpo/index.jsx
@@ -1,24 +1,50 @@
-const GifsExpo= ({categories = []} ) => {
-    let getGifs = (categories ) => {
-        const gifsList = []
+import { useState } from "react"
 
-        categories.forEach(async (category)=>{
+const GifsExpo= ({categories = []} ) => {
+    const [urlList,setUrlList] = useState([])
+    const getGifs = async (categories ) => {
+     //   let    gifsList = []
+
+       const responsesList= await Promise.all(categories.map(async (category)=>{
         const response = await fetch (
          `https://api.giphy.com/v1/gifs/search?api_key=wS0Wxs0FN0c3gVE3yrVSHlCmT40M7W3G&q=${categories}&limit10`
             )
         const apiResponse = await response.json()
-        gifsList =apiResponse.data.map((gif)=>{
-            return gif.images.fixed_width.url
-        })
+        return apiResponse.data
+        //gifsList =apiResponse.data.map((gif)=>{
+          //  return gif.images.fixed_width.url
+        //})
+        
        
-        } )
-        console.log(gifsList)
+        }))
+        let gifsList =[]
+         responsesList.forEach((data)=>{
+            data.forEach((item) =>{
+                gifsList = [...gifsList,item.images.fixed_width.url.split('?')[0]]
+
+            })
+         })
+        
+        setUrlList([...gifsList])
 
             }
     getGifs(categories)
+
+
     
     return(
+        <>
         <h4> GifsExpo</h4>
+        <ol>
+            {
+             urlList.map((url) => {
+                return (
+                <li key={url}>{url}</li>
+                )
+             })     
+            }
+               </ol>
+               </>
     )
 }
 export default GifsExpo
